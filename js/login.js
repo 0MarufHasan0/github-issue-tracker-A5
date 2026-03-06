41399014d56dbd29b2be4eb22254ac69eda6622f
0MarufHasan0 <marufhasan11.11.2323@gmail.com>
2026-03-06 23:33:42 +0600

    login.js done

---
+++ js/login.js
@@ -0,0 +1,31 @@
+console.log('hello world')
+
+document.getElementById('login-btn')
+.addEventListener('click', function(){
+
+    // userName value
+    const UserInput = document.getElementById('input-username');
+    const UserValue = UserInput.value ;
+    console.log(UserValue)
+
+    // password value
+
+
+    const userPass= document.getElementById("input-password") ;
+    const passValue = userPass.value
+    console.log(passValue)
+
+// condition
+
+if(UserValue ==="admin" && passValue === "admin123" ){
+    alert('login success')
+
+    window.location.assign('./index.html')
+}
+
+else{
+    alert('login fail')
+    return
+}
+
+})

e21d4c10e50e7919b2588c1d62234e8d5aeffc39
0MarufHasan0 <marufhasan11.11.2323@gmail.com>
2026-03-08 16:37:39 +0600

    modal added

--- js/scipt.js
+++ js/scipt.js
@@ -5,10 +5,25 @@ const loadingSpinner = document.getElementById('loading-spinner')
 const allBtn = document.getElementById('btn-all')
 const btnOpen = document.getElementById('btn-open')
 const btnClosed = document.getElementById('btn-closed')
+const showIssueModal = document.getElementById('my_modal_5')
+const modalId = document.getElementById('modal')
 
 
 let currentStatus = []
 
+
+ // label array
+
+const createElements=(arr)=>{
+
+    const htmLElement = arr.map(el => `<button class=" flex  justify-center items-center gap-1 ${el ==='bug'?'text-error border border-[#FECACA] bg-[#FECACA]/40 ' : el==='documentation'?'bg-gray-300 text-yellow'
+       : el =='help wanted'? 'bg-warning/40 text-warning' : 
+       el === 'enhancement' ? 'bg-success/50 text-success' :'bg-secondary/40 text-secondary'}  px-3 py-1  rounded-md "> <img src="${el==='bug'?'./assets/BugDroid.png': el==='help wanted'?'./assets/Vector.png' :'./assets/Sparkle.png' } " alt="" >  ${el.toUpperCase()}</button>`)
+
+    // console.log(htmLElement)
+     return htmLElement.join('');
+
+}
 
 
 
@@ -19,7 +34,7 @@ let currentStatus = []
 // all isue load and display
 
 async function alIssue() {
-
+    
     loadingSpinner.classList.remove('hidden')
     loadingSpinner.classList.add('flex')
 
@@ -35,32 +50,17 @@ async function alIssue() {
 
 
 
-
-
 // display issue card
 
 function displayIssues(issues){
 
-   // array
+   // issues calculation
 
-const createElements=(arr)=>{
-
-    const htmLElement = arr.map(el => `<button class=" flex  justify-center items-center gap-1 ${el ==='bug'?'text-error border border-[#FECACA] bg-[#FECACA]/40 ' : el==='documentation'?'bg-gray-300 text-yellow'
-       : el =='help wanted'? 'bg-warning/40 text-warning' : 
-       el === 'enhancement' ? 'bg-success/50 text-success' :'bg-secondary/40 text-secondary'}  px-3 py-1  rounded-md "> <img src="${el==='bug'?'./assets/BugDroid.png': el==='help wanted'?'./assets/Vector.png' :'./assets/Sparkle.png' } " alt="" >  ${el.toUpperCase()}</button>`)
-
-    // console.log(htmLElement)
-     return htmLElement.join('');
-
-}
-
-    
-
-    // issues calculation
-
-    
  const totalCount = issues.length
+
+
 //    console.log(totalCount)
+
  issueCount.innerText = `${totalCount} Issues`
 
     // console.log(issues)
@@ -71,13 +71,15 @@ const createElements=(arr)=>{
 
 
         const cardIssues = document.createElement('div')
+
+
         cardIssues.innerHTML=`
         
         
         
-       <div class="p-6 ${issue.status ==='open'?'border-t-2  border-[#00A96E]':'border-t-2 border-purple-700'} shadow-lg rounded-xl space-y-2">
+       <div onclick="loadIssuesDetail('${issue.id}')" class="p-6 ${issue.status ==='open'?'border-t-2  border-[#00A96E]':'border-t-2 border-purple-700'} shadow-lg rounded-xl space-y-2">
 
-        <div class="flex justify-between items-center">
+        <div class="flex justify-between items-center"  >
         <img src="${issue.status==='open'?'./assets/Open-Status.png' : './assets/Closed- Status .png'}" class="h-6">
         <p class="${ issue.priority==='high'?'bg-red-100 text-[#EF4444]': issue.priority === 'medium'?'bg-warning/20 text-warning': 'bg-neutral-content text-gray font-bold'} px-2 py-1 rounded-md text-xs font-bold">${issue.priority}</p>
         </div>
@@ -121,6 +123,72 @@ const createElements=(arr)=>{
 
 
 
+// Modal
+
+  const loadIssuesDetail= async(id)=>{
+    
+
+     const res = await fetch (`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+     const data = await res.json();
+     
+
+     displayIssueModal(data.data)
+    
+
+    showIssueModal.showModal()
+
+
+}
+
+const displayIssueModal =(issue) =>{
+
+    // array
+
+
+
+
+    modalId.innerHTML=`
+    
+    <h3 class="text-lg font-bold">${issue.title}</h3>
+    
+    <div class="flex items-center gap-3 mt-1 ">
+      <div class="badge ${issue.status=='open'?'badge-success':'badge-error'}  text-white">${issue.status}</div>
+      <p class="text-gray-400 text-xs">. Opened by ${issue.author} .</p>
+      <span class="text-gray-400 text-xs" >${new Date(issue.updatedAt).toLocaleDateString('en-US')}</span>
+    </div>
+
+    <div class="flex gap-2 pt-7">
+
+        ${ createElements(issue.labels)}
+
+    </div>
+
+    <p  class="  text-gray-400 text-xs pt-5">${issue.description}</p>
+
+    <div class="flex  gap-50 items-center pt-8">
+
+      <div class="space-y-1 " >
+        <p  class="  text-gray-400 text-xs">Assignee:</p>
+        <h4 class="font-bold text-xl" >${issue.assignee ? issue.assignee : ''}</h4>
+
+      </div>
+
+      <div class="space-y-1">
+        <p  class="  text-gray-400 text-xs">Priority:</p>
+        <div class="badge ${ issue.priority==='high'?'bg-red-100 text-[#EF4444]': issue.priority === 'medium'?'bg-warning/20 text-warning': 'bg-neutral-content text-gray font-bold'}">${issue.priority}</div>
+      </div>
+
+
+    </div>
+    
+    `
+}
+
+
+
+
+
+
 
 
 //  search
@@ -140,11 +208,6 @@ document.getElementById('btn-search').addEventListener('click',async ()=>{
  displayIssues(data.data)
       
 })
-
-
-
-
-
 
 
 
@@ -181,6 +244,7 @@ document.getElementById('btn-search').addEventListener('click',async ()=>{
     if(id === 'btn-all'){
 
       displayIssues(currentStatus)
+
     } else if (id === 'btn-open'){
         const OpenIssue = currentStatus.filter(issue => issue.status ==='open')
     
